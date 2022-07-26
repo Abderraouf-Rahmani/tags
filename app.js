9b80ff7df8a78cd62803546ccd9067ffcff965ae
Abderraouf-Rahmani <95105187+Abderraouf-Rahmani@users.noreply.github.com>
2022-07-26 14:46:05 +0100

    error handling, control over number of tags and tag length

--- app.js
+++ app.js
@@ -3,13 +3,45 @@ let firstInputRegExp = /[a-z0-9]{1}/i;
 let tag = [];
 let tags = [];
 let TagString = "";
-let tagsLimitNum = 4;
+let tagsLimitNum = 6;
+let tagLength = 25;
 let tagsContainer = document.getElementById("tags-container");
 let tagSpan = document.getElementById("tag-span");
 let tagSpanClose = document.getElementById("tagSpanClose");
+let inputCon = document.getElementById("input-container");
+let isTooLong = false;
+tagsInput.placeholder = `Add up to ${tagsLimitNum} tags...`;
+
+const say = (msg, msgType) => {
+  let say = document.getElementById("say");
+
+  switch (msgType) {
+    case "error":
+      say.style.color = "red";
+      break;
+    case "success":
+      say.style.color = "green";
+      inputCon.style.outline = "2px solid #79db9d";
+      break;
+    case "alert":
+      say.style.color = "#F7C600";
+      break;
+
+    default:
+      break;
+  }
+
+  say.textContent = msg;
+};
 
 const addToTagSpan = (TAGS) => {
   tagsContainer.innerHTML = "";
+  if (TAGS.length >= 0 && TAGS.length < tagsLimitNum) {
+    tagsInput.placeholder = `Add up to ${tagsLimitNum} tags...`;
+  } else {
+    tagsInput.placeholder = `you've got all the ${tagsLimitNum} tags :)`;
+    say("", "success");
+  }
   TAGS.forEach((TAG, i) => {
     let newSpanCon = document.createElement("span");
     newSpanCon.id = "tag-container";
@@ -44,23 +76,27 @@ const handleKeyDown = (e) => {
       tag = [...TagString];
       tags.pop();
       addToTagSpan(tags);
-      tagsInput.placeholder = "Add up to 4 tags...";
       return;
     }
+    isTooLong = false;
     tag.pop();
     TagString = tag.join("");
     tagsInput.value = TagString;
   }
 
   if (tags.length >= tagsLimitNum) {
-    tagsInput.placeholder = "you've got all the 4 tags :)";
     return;
   }
 
   if (
     (inputType === "insertLineBreak" || input === " " || input === ",") &&
-    TagString !== ""
+    TagString !== "" &&
+    !isTooLong
   ) {
+    if (tags.includes(TagString)) {
+      say("you already have this tag", "alert");
+      return;
+    }
     tags.push(TagString);
     addToTagSpan(tags);
     tagsInput.value = "";
@@ -70,9 +106,14 @@ const handleKeyDown = (e) => {
 
   if (inputType === "insertText") {
     if (!input.match(firstInputRegExp)) return;
-    tag.push(input);
-    TagString = tag.join("");
-    tagsInput.value = TagString;
+    if (tag.length <= tagLength) {
+      tag.push(input);
+      TagString = tag.join("");
+      tagsInput.value = TagString;
+    } else {
+      say(`a tag can't have more than ${tagLength} characters`, "error");
+      isTooLong = true;
+    }
   }
 };
 
